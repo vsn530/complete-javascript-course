3bf05deb3b73bf19319f7b3c141c3d9f0d26e2e1
svaddineni <svaddineni@office.zone@qindl023.office.zone>
2021-06-23 12:14:36 +0530

    variables practice

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,17 @@
+// 
+console.log('assignments.js is running');
+
+let country = 'India';
+let continent = 'Asia';
+let population = 1000;
+
+console.log(`I am from ${country} which is in ${continent} continent, Our country population is more than ${population} millions`);
+
+
+
+// 
+setTimeout(() => {
+    console.log('running after 1 second from assignments.js');
+}, 1000);
+
+console.log('End of assignments.js');
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,16 @@
+console.log('script.js is running');
+// alert('Hello World!!')
+let js= 'amazing';
+if(js === 'amazing'){
+//   alert('javascript is fun');
+}
+
+console.log(23+3-5);
+
+let firstName = 'Srinivas';
+
+console.log(firstName);
+
+let PI = 3.14;
+console.log(PI);
+
